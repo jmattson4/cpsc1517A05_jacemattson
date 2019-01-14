f32a2646c10822fa6d9a9d0082b7505b927f7859
jmattson4 <jmattson4@studentmail.nait.ca>
2019-01-14 16:54:08 -0700

    Update Turn.cs
    updated the turn file

--- OOPpracticesolution/OOPpractice/Turn.cs
+++ OOPpracticesolution/OOPpractice/Turn.cs
@@ -8,5 +8,61 @@ namespace OOPpractice
 {
     class Turn
     {
+        private Die[,] _TrackTurn = new Die[,] { };
+
+
+        private int _TurnTracker;
+
+        public Die[,] TrackTurn
+        {
+            get { return _TrackTurn; }
+            set
+            {
+                _TrackTurn = value;
+            }
+        }
+
+        public Turn(Die playerOneTurn, Die playerTwoTurn)
+        {
+
+            TrackTurn[_TurnTracker, _TurnTracker] = playerOneTurn, playerTwoTurn;
+            TrackTurn[_TurnTracker, _TurnTracker + 1] = playerTwoTurn;
+            _TurnTracker++;
+        }
+
+
+        public void AddTurn(Die playerTurn)
+        {
+            TrackTurn[_TurnTracker, _TurnTracker] = playerTurn;
+
+            _TurnTracker++;
+        }
+        public void DisplayTurns()
+        {
+            Console.WriteLine(TrackTurn.ToString());
+        }
     }
+    //class Turn1
+    /*{
+     * private int _Player1Roll;
+     * 
+     * public int Player1Roll
+     *      {
+     *         
+     *      get
+     *          {
+     *              return _Player1Roll;
+     *          }
+     *      
+     *      set
+     *          {
+     *              _Player1Roll = value;
+     *          }          
+     *      }
+     *      
+     *  public int Player2Roll {get; set; }
+     *      
+     */
+    //
+    // }
 }
